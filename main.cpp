2e7aa83aaf4549e4045bdd291c949b860ec6cf11
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2025-08-02 01:44:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 
 int main()
 {
+	createLogFile();
+
 	//Локализация
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	setlocale(LC_ALL, "Ru");
 
+
 	int ClientCounter = 0;
 
-	Client client_1("@Test", 1000, 60, notStarted, "V16 - N13", ClientCounter++);
-	std::cout << ClientCounter;
-	client_1.set_ID("@User_001");
-	std::cout << client_1.get_ID() << "\n";
-	client_1.set_ID();
-	std::cout << client_1.get_ID() << "\n";
+	Client client_1( "@Test", 1000, 60, inProgress_notPayed, "V16 - N13", ClientCounter++ );
+
 	client_1.set_workState();
-	std::cout << client_1.get_workState() << "\n";
+	client_1.get_workState();
+
 }
