ed9ab9e14b92a4cb1ece50e9a5fa916b50b43302
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2025-08-02 01:42:59 +0300

    Create Logs.cpp

---
+++ Logs.cpp
@@ -0,0 +1,66 @@
+#include "Logs.h"
+std::ofstream currentLogFile;
+
+//Создания файла с логом
+void createLogFile()
+{
+
+  std::time_t now = std::time(nullptr);
+  struct tm tstruct;
+
+  if (localtime_s(&tstruct, &now) != 0)
+  {
+    std::cerr << "Ошибка получения времени." << std::endl;
+    return;
+  }
+
+  std::stringstream ss;
+  ss << std::put_time(&tstruct, "%Y %m %d %H %M %S");
+  std::string formattedTime;
+  formattedTime = ss.str();
+
+  std::string LogName_part1 = "Log [";
+  std::string LogName_part2 = formattedTime;
+  std::string LogName_part3 = "].txt";
+
+  std::string LogFileName = LogName_part1 + LogName_part2 + LogName_part3;
+  currentLogFile.open(LogFileName);
+}
+
+//Вывод сообщения с временем
+void printLog(std::string LogMessage)
+{
+  std::time_t now = std::time(nullptr);
+  struct tm tstruct;
+
+  if (localtime_s(&tstruct, &now) != 0)
+  {
+    std::cerr << "Ошибка получения времени." << std::endl;
+    return;
+  }
+
+  std::stringstream ss;
+  ss << std::put_time(&tstruct, "%Y-%m-%d %H:%M:%S");
+  std::string formattedTime = ss.str();
+  std::cout << "[" << formattedTime << "]: " << LogMessage << "\n";
+  currentLogFile << "[" << formattedTime << "]: " << LogMessage << "\n";
+}
+
+//Вывод времени 
+void printLog()
+{
+  std::time_t now = std::time(nullptr);
+  struct tm tstruct;
+
+  if (localtime_s(&tstruct, &now) != 0)
+  {
+    std::cerr << "Ошибка получения времени" << std::endl;
+    return;
+  }
+
+  std::stringstream ss;
+  ss << std::put_time(&tstruct, "%Y-%m-%d %H:%M:%S");
+  std::string formattedTime = ss.str();
+  std::cout << "[" << formattedTime << "] ";
+  currentLogFile <<  "[" << formattedTime << "] ";
+}
