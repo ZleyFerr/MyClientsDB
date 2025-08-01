0643098f9f315e5b75c77b838007555eec56ccad
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2025-08-02 01:43:14 +0300

    Create Logs.h

---
+++ Logs.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <chrono>
+#include <iomanip> 
+#include <sstream> 
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+
+void printLog( std::string LogMessage );
+void printLog();
+void createLogFile();
