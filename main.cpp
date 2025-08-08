65b4d0497e67b1b5cc7941c05d89b6e5a2d79fb7
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2025-08-09 01:06:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,8 @@
-#include <windows.h>
-#include "Client.h"
+#include "menu.h"
 
 int main()
 {
-	createLogFile();
-
-	//Локализация
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	setlocale(LC_ALL, "Ru");
-
-
-	int ClientCounter = 0;
-
-	Client client_1( "@Test", 1000, 60, inProgress_notPayed, "V16 - N13", ClientCounter++ );
-
-	client_1.set_workState();
-	client_1.get_workState();
-
+	createLogFile(); //Создание файла логов
+	setupDB(); // Начало работы с базой данных
+	menuStart();
 }
