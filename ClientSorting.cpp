b4a78b59caf2441f780ce1e70fdfba72c8a82223
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2025-08-23 14:35:35 +0300

    Update ClientSorting.cpp

--- ClientSorting.cpp
+++ ClientSorting.cpp
@@ -14,3 +14,18 @@ bool sort_by_timeElapsedMinutes(const Client a, const Client b)
 {
 	return a.get_timeElapsed_minutes() < b.get_timeElapsed_minutes();
 }
+
+bool sort_by_amount(const amountOfOrdersTop a, const amountOfOrdersTop b)
+{
+	return a.amountOfOrders < b.amountOfOrders;
+}
+
+bool sort_by_payment_top(const paymentTop a, const paymentTop b)
+{
+	return a.payment < b.payment;
+}
+
+bool sort_by_timeElapsed_top(const timeElapsedTop a, const timeElapsedTop b)
+{
+	return a.timeElapsed < b.timeElapsed;
+}
