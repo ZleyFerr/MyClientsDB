be1de44a00a8bad4b708445676540ddb0a4c17b3
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2025-08-23 14:35:44 +0300

    Update ClientSorting.h

--- ClientSorting.h
+++ ClientSorting.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "Client.h"
 #include <algorithm>
+#include "DataBase_TopCreator.h"
 
 bool sort_by_payment(const Client a, const Client b);
 bool sort_by_ID(const Client a, const Client b);
 bool sort_by_timeElapsedMinutes(const Client a, const Client b);
-
+bool sort_by_amount(const amountOfOrdersTop a, const amountOfOrdersTop b);
+bool sort_by_payment_top(const paymentTop a, const paymentTop b);
+bool sort_by_timeElapsed_top(const timeElapsedTop a, const timeElapsedTop b);
